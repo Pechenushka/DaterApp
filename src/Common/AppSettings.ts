7a6fa0e5d63a2745482a24c0632e90a568b777e4
StasMetenkoWeWork <stas.metenko@wework.com>
2022-05-16 21:39:58 +0300

    prod;

--- src/Common/AppSettings.ts
+++ src/Common/AppSettings.ts
@@ -1,8 +1,8 @@
 class AppSettings {
-  private prod = false;
+  private prod = true;
   public get apiEndpoint() {
     if (this.prod) {
-      return 'http://dater.na4u.ru/';
+      return 'http://142.132.168.38:3001/';
     }
     return 'https://e2c9-194-39-226-155.eu.ngrok.io/';
   }
