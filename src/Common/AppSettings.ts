e01ee623b93bbafe43fd25cb080828a930208dc0
Stanislav Metenko <smetenko@onecallcm.com>
2022-08-18 15:27:23 +0300

    keys and release bundle;

--- src/Common/AppSettings.ts
+++ src/Common/AppSettings.ts
@@ -1,8 +1,8 @@
 class AppSettings {
-  private prod = false;
+  private prod = true;
   public get apiEndpoint() {
     if (this.prod) {
-      return 'http://142.132.168.38:3001/';
+      return 'http://116.202.31.73:3001/';
     }
     return 'http://192.168.0.105:3001/';
   }
