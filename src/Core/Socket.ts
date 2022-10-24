1a1e6ec941654fa687f2d82df18fbffc8a979a99
Stanislav Metenko <smetenko@onecallcm.com>
2022-10-24 12:26:30 +0300

    fix socket;

--- src/Core/Socket.ts
+++ src/Core/Socket.ts
@@ -3,18 +3,22 @@ import {appSettings} from '../Common/AppSettings';
 import {app} from './AppImpl';
 
 class SocketHandler {
-  public static socket: any;
+  public static socket: any = null;
   constructor() {}
 
   public static connect = () => {
-    SocketHandler.socket = io(appSettings.apiEndpoint, {
-      query: {id: app.currentUser ? app.currentUser.userId : -1},
-    });
-    SocketHandler.socket.connect();
+    if (app.currentUser && app.currentUser.userId !== -1) {
+      SocketHandler.socket = io(appSettings.apiEndpoint, {
+        query: {id: app.currentUser.userId, name: app.currentUser.userName},
+      });
+      SocketHandler.socket.connect();
+    }
   };
 
   public static disconnect = () => {
-    SocketHandler.socket.disconnect();
+    if (SocketHandler.socket !== null) {
+      SocketHandler.socket.disconnect();
+    }
   };
 }
 
